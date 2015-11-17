5ea932b953d3ba6496985e8a5732086f351919b9
Jikoo <Jikoo.Games@gmail.com>
2015-11-17 14:41:02 -0500

    Set up post-enable, can't depend on all the plugis in the world. Actually prevent NPE when no commands are declared in plugin.yml Changed project name in pom.xml, the goal is no longer only to fix Essentials

--- src/main/java/com/github/jikoo/permissionssetup/PermissionsSetup.java
+++ src/main/java/com/github/jikoo/permissionssetup/PermissionsSetup.java
@@ -1,58 +1,66 @@
 package com.github.jikoo.permissionssetup;
 
-import java.util.Collection;
 import java.util.IllegalFormatException;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PermissionsSetup extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		saveDefaultConfig();
+		this.saveDefaultConfig();
 
-		ConfigurationSection plugins = this.getConfig().getConfigurationSection("plugins");
-		for (String section : plugins.getKeys(false)) {
+		new PostEnableSetup().runTask(this);
 
-			ConfigurationSection pluginSection = plugins.getConfigurationSection(section);
+	}
 
-			// Ensure a valid format is created
-			if (!pluginSection.isString("format")) {
-				this.getLogger().warning("No permission format set for plugin " + section + ", skipping!");
-				continue;
+	private class PostEnableSetup extends BukkitRunnable {
+
+		public void run() {
+			ConfigurationSection plugins = getConfig().getConfigurationSection("plugins");
+			for (String section : plugins.getKeys(false)) {
+	
+				ConfigurationSection pluginSection = plugins.getConfigurationSection(section);
+	
+				// Ensure a valid format is created
+				if (!pluginSection.isString("format")) {
+					getLogger().warning("No permission format set for plugin " + section + ", skipping!");
+					continue;
+				}
+				String format = pluginSection.getString("format");
+				try {
+					String.format(format, "test");
+				} catch (IllegalFormatException e) {
+					getLogger().warning("Invalid format set for plugin " + section + ", skipping!");
+					continue;
+				}
+	
+				Plugin plugin = getServer().getPluginManager().getPlugin(section);
+	
+				// Is plugin enabled? Null check is handled by instanceof.
+				if (plugin instanceof JavaPlugin) {
+					addCommandPermissions((JavaPlugin) plugin, format, pluginSection.getStringList("wildcards"));
+				}
 			}
-			String format = pluginSection.getString("format");
-			try {
-				String.format(format, "test");
-			} catch (IllegalFormatException e) {
-				this.getLogger().warning("Invalid format set for plugin " + section + ", skipping!");
-				continue;
-			}
-
-			Plugin plugin = getServer().getPluginManager().getPlugin(section);
-
-			// Is plugin enabled? Null check is handled by instanceof.
-			if (!(plugin instanceof JavaPlugin)) {
-				continue;
-			}
-
-			addCommandPermissions((JavaPlugin) plugin, format, pluginSection.getStringList("wildcards"));
 		}
+
 	}
 
 	private void addCommandPermissions(JavaPlugin plugin, String format, List<String> wildcards) {
-		Collection<String> commands = plugin.getDescription().getCommands().keySet();
-
-		if (commands.isEmpty()) {
+		Map<String, Map<String, Object>> commands = plugin.getDescription().getCommands();
+		if (commands == null || commands.isEmpty()) {
+			getLogger().warning(plugin.getName() + " does not have any commands registered in plugin.yml!");
 			return;
 		}
 
-		for (String command : commands) {
+		for (String command : commands.keySet()) {
 			String perm = String.format(format, command);
 			// Note: Bukkit#getPluginCommand will return a different plugin's command if overridden.
 			// This method will always get the correct command.
@@ -61,10 +69,10 @@ public class PermissionsSetup extends JavaPlugin {
 			for (String wildcard : wildcards) {
 				Permission child;
 				try {
-					child = new Permission(perm, PermissionDefault.FALSE);
+					child = new Permission(perm, PermissionDefault.OP);
 					getServer().getPluginManager().addPermission(child);
 				} catch (IllegalArgumentException e) {
-					child = getServer().getPluginManager().getPermission(perm);
+					child = this.getServer().getPluginManager().getPermission(perm);
 				}
 				child.addParent(wildcard, true);
 			}
